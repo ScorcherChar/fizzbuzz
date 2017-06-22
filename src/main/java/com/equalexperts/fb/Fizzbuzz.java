66c23cc6b711ff2a28ac944e863c4f007899f4cc
Thomas Squires <mjollnir8@gmail.com>
2017-06-22 20:26:49 +0200

    formatting tidy up

--- src/main/java/com/equalexperts/fb/Fizzbuzz.java
+++ src/main/java/com/equalexperts/fb/Fizzbuzz.java
@@ -8,13 +8,14 @@ public class Fizzbuzz {
     /**
      * Creates a string counting from one to the given max replacing every third number with fiz, 5th with buzz and
      * fifteenth with fizzbuzz. Any number containing 3 will return lucky
+     *
      * @param max the max number to count to. Expected to be one or more.
      * @return the output string.
-     * @exception IllegalArgumentException On input error.
+     * @throws IllegalArgumentException On input error.
      * @see IllegalArgumentException
      */
-    public static String createOutput(final Integer max) throws IllegalArgumentException{
-        if(max == null || max < 1){
+    public static String createOutput(final Integer max) throws IllegalArgumentException {
+        if (max == null || max < 1) {
             throw new IllegalArgumentException("Invalid max number given to fizzbuzz. Expecting above one");
         }
 
@@ -22,16 +23,16 @@ public class Fizzbuzz {
         return numbers.map(x -> toFizzbuzzWord(x)).reduce((s1, s2) -> s1 + " " + s2).get();
     }
 
-    private static String toFizzbuzzWord(final Integer inputNumber){
-        if(containsAThree(inputNumber)){
+    private static String toFizzbuzzWord(final Integer inputNumber) {
+        if (containsAThree(inputNumber)) {
             return "lucky";
-        }else if(isExactMultipleOf(inputNumber, 15)){
+        } else if (isExactMultipleOf(inputNumber, 15)) {
             return "fizzbuzz";
-        }else if(isExactMultipleOf(inputNumber, 5)){
+        } else if (isExactMultipleOf(inputNumber, 5)) {
             return "buzz";
-        }else if(isExactMultipleOf(inputNumber, 3)){
+        } else if (isExactMultipleOf(inputNumber, 3)) {
             return "fizz";
-        }else {
+        } else {
             return inputNumber.toString();
         }
     }
--- src/test/java/com/equalexperts/fb/FizzbuzzTest.java
+++ src/test/java/com/equalexperts/fb/FizzbuzzTest.java
@@ -8,24 +8,24 @@ import static org.junit.Assert.assertTrue;
 public class FizzbuzzTest {
 
     @Test(expected = IllegalArgumentException.class)
-    public void nullInputGivesArgumentException(){
+    public void nullInputGivesArgumentException() {
         Fizzbuzz.createOutput(null);
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void zeroInputGivesArgumentException(){
+    public void zeroInputGivesArgumentException() {
         Fizzbuzz.createOutput(0);
     }
 
     @Test
-    public void twentyGivesExpectedOutput(){
+    public void twentyGivesExpectedOutput() {
         String output = Fizzbuzz.createOutput(20);
         final String expected = "1 2 lucky 4 buzz fizz 7 8 fizz buzz 11 fizz lucky 14 fizzbuzz 16 17 fizz 19 buzz";
         assertEquals(expected, output);
     }
 
     @Test
-    public void twoDigitNumbersWithThreeGiveLucky(){
+    public void twoDigitNumbersWithThreeGiveLucky() {
         String output = Fizzbuzz.createOutput(30);
         assertTrue(output.endsWith("lucky"));
     }
